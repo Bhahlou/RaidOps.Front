3b67df49e50439695562b70b9a40f4c11da2c87f
Bhahlou <bhahlou@gmail.com>
2026-05-21 13:03:34 +0200

    Make tab logo environment dependant

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LanguageService } from './core/services/language.service';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +13,12 @@ import { LanguageService } from './core/services/language.service';
 export class App {
   constructor() {
     inject(LanguageService);
+
+    const favicon = inject(DOCUMENT).querySelector<HTMLLinkElement>('link[rel="icon"]');
+    if (favicon) {
+      favicon.href = environment.production
+        ? 'assets/Logo no background.svg'
+        : 'assets/Logo no background DEV.svg';
+    }
   }
 }
